b47701cc0acecc4c6c0bde568a4263c26c8280ef
chaotzu <darkcore.7@gmail.com>
2023-03-01 13:24:50 -0600

    1.0

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿//Write a text file - Version-1
+using System;
+using System.IO;
+namespace readwriteapp
+{
+    class Filescsharp
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            String linea;
+            Boolean encontrado = false;
+            try
+            {   
+                //Ruta del documento de texto donde se escribe
+                //Agregar
+                //StreamWriter sw = File.AppendText("Test.txt");
+
+
+                //Sobreescribir
+                StreamWriter sw = new StreamWriter("Test.txt");
+                
+                //Escribir linea en documento
+                sw.WriteLine("Escritura 1");
+                //Escribir otra linea en documento
+                sw.WriteLine("ISV0101");                
+                //Cerrar archivo
+                sw.Close();
+
+                //Ruta del documento de texto a leer
+                StreamReader sr = new StreamReader("Test.txt");
+                //Leer primer linea
+                linea = sr.ReadLine();
+                //Continuar leyendo hasta el final del archivo
+                while (linea != null)
+                {
+                    if(linea == "ISV0101")
+                        encontrado = true;                    
+                    //Escribir linea en consola
+                    Console.WriteLine(linea);
+                    //Leer linea siguiente
+                    linea = sr.ReadLine();
+                }
+                //Cerrar archivo
+                sr.Close();
+                Console.ReadLine();
+                if(encontrado == true)
+                    Console.WriteLine("Si existe");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Bloque final.");
+            }
+        }
+    }
+}
